ba3f0cacb3d27330a1e778ba305a344dd5d707ee
PJ Oschmann <phillip236.po@gmail.com>
2023-02-13 12:43:07 -0500

    Increase Loading Anim Performance
    - Loading animation now changes opacity instead of background color
    - All buttons use just one animation, so no JS if logic needed

--- script-templatehtml.js
+++ script-templatehtml.js
@@ -4,16 +4,19 @@ console.log("Loaded!");
 function doLoadingAnim(btnId) {
   console.log("Clicked");
   btn = document.getElementById(btnId);
+  btn.setAttribute("style", "animation: loadingAnim 4s forwards infinite; animation-delay: 1s; transform: translate(0, 4px); box-shadow: 0px 0px #000000;")
   
+  //Old Loading Styles:
+  /*
   if (btn.getAttribute("class") === "btnNormal" ){
     btn.setAttribute("style", "animation: normalLoading 4s forwards infinite; animation-delay: 1s; transform: translate(0, 4px); box-shadow: 0px 0px #000000;")
   }
   else if (btn.getAttribute("class") === "btnHighlight") {
-      btn.setAttribute("style","animation: highlightLoading 4s forwards infinite; animation-delay: 1s; transform: translate(0, 4px); box-shadow: 0px 0px #000000;");
+      btn.setAttribute("style","animation: highlightLoadingNew 4s forwards infinite; animation-delay: 1s; transform: translate(0, 4px); box-shadow: 0px 0px #000000;");
   }
   else {
     btn.setAttribute("style","animation: spicyLoading 4s forwards infinite; animation-delay: 1s; transform: translate(0, 4px); box-shadow: 0px 0px #000000;");
   }
-  
+  */
 
 }
